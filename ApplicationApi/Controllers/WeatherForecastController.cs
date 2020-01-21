e1fe5cd1d3607a82f911472829388b974b1dd132
jhon-vargas-sf <jhon.vargas@salesforce.com>
2020-01-21 08:31:59 -0500

    Commit test

--- ApplicationApi/Controllers/WeatherForecastController.cs
+++ ApplicationApi/Controllers/WeatherForecastController.cs
@@ -11,6 +11,7 @@ namespace ApplicationApi.Controllers
     [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
     {
+        //TODO: test jh
         private static readonly string[] Summaries = new[]
         {
             "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
