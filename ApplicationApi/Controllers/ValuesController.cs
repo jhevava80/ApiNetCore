349bc7070b3a198b994ee7f8155e1f0f4a0fb07d
jhonv80 <jhevava80@gmail.com>
2020-03-21 18:59:42 -0500

    it becomed into asynchronous methods

--- ApplicationApi/Controllers/ValuesController.cs
+++ ApplicationApi/Controllers/ValuesController.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ApplicationApi.Data;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace ApplicationApi.Controllers
 {
@@ -10,18 +12,27 @@ namespace ApplicationApi.Controllers
     [ApiController]
     public class ValuesController : Controller
     {
+        private readonly DataContext dataContext;
+
+        public ValuesController(DataContext dataContext)
+        {
+            this.dataContext = dataContext;
+        }
+
         // GET api/values
         [HttpGet]
-        public ActionResult<IEnumerable<string>> Get()
+        public async Task<IActionResult> GetValues()
         {
-            return new string[] { "value1", "value2" };
+            var values = await this.dataContext.Values.ToListAsync();
+            return Ok(values);
         }
 
         // GET api/values/5
         [HttpGet("{id}")]
-        public ActionResult<string> Get(int id)
+        public async Task<IActionResult> GetValue(int id)
         {
-            return "value";
+            var value = await this.dataContext.Values.FirstOrDefaultAsync(x=> x.Id == id);
+            return Ok(value);
         }
 
         // POST api/values
