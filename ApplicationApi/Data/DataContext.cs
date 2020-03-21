4358faaff49c8a93e801549a209706ff98c43e5e
jhonv80 <jhevava80@gmail.com>
2020-03-21 18:30:27 -0500

    Added Entity Framework

---
+++ ApplicationApi/Data/DataContext.cs
@@ -0,0 +1,16 @@
+﻿using ApplicationApi.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApplicationApi.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options){}
+        public DbSet<Value> Values { get; set; }
+
+    }
+}
